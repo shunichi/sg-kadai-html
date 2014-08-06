02132731c7a871320bc101537521ee174da4c813
Shunichi Ikegami <sike.tm@gmail.com>
2014-08-06 17:28:56 +0900

    Add slide-fade effects.

--- html/js/js-menu.js
+++ html/js/js-menu.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	$("#search-tool").click(function(){
-		$("#menu").fadeToggle("fast");
-		$("#results").fadeToggle("fast");
-		$("#search-tool").toggleClass("toggle-button-selected")
+		$(".menu").fadeToggle("fast");
+		$(".menu").toggleClass("menu-opened");
+		$(".results").fadeToggle("fast");
+		$(".results").toggleClass("results-closed");
+		$("#search-tool").toggleClass("toggle-button-selected");
 	});
 });
